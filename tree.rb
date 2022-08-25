1f3dc0fd9d95100c49a55d583b5e9537f50522e9
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-25 11:54:16 -0500

    Complete insert implementation.

--- tree.rb
+++ tree.rb
@@ -10,6 +10,18 @@ class Tree
     @root = build_tree(array, 0, array.length - 1)
   end
 
+  def insert(key, node = @root)
+    return Node.new(key) if node.nil?
+    return node if node.value == key
+
+    if key < node.value
+      node.left = insert(key, node.left)
+    else
+      node.right = insert(key, node.right)
+    end
+    node
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
@@ -31,5 +43,13 @@ class Tree
   end
 end
 
-tree = Tree.new([0, 1, 2, 3, 4, 5, 6])
+tree = Tree.new([-11, -7, -6, 0, 1.5, 4, 7, 18, 27.5, 30, 30.1,
+                 30.11, 1500, 1501])
+tree.pretty_print
+tree.insert(5)
+tree.insert(6)
+tree.pretty_print
+tree.insert(7)
+tree.pretty_print
+tree.insert(8)
 tree.pretty_print
