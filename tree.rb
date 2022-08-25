468bc2cb1f7f7537c838e9d0acdaedf111ebcb33
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-25 15:12:10 -0500

    Create minimum_value method and inorder_successor.

--- tree.rb
+++ tree.rb
@@ -4,7 +4,7 @@ require_relative 'node'
 
 # Class for a balanced binary search tree
 class Tree
-  attr_reader :root
+  attr_accessor :root
 
   def initialize(array)
     @root = build_tree(array, 0, array.length - 1)
@@ -20,6 +20,21 @@ class Tree
       node.right = insert(key, node.right)
     end
     node
+  end
+
+  def inorder_successor(node = @root)
+    unless node.right.nil?
+      right_subtree = node.right
+      return minimum_value(right_subtree)
+    end
+    minimum_value(node)
+  end
+
+  def minimum_value(node = @root)
+    return nil if node.nil?
+    return node.value if node.left.nil?
+
+    minimum_value(node.left)
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -43,13 +58,7 @@ class Tree
   end
 end
 
-tree = Tree.new([-11, -7, -6, 0, 1.5, 4, 7, 18, 27.5, 30, 30.1,
-                 30.11, 1500, 1501])
+tree = Tree.new([-8, -7, -6, -5, -4, -3, -2, -1, 1, 3, 4, 6])
 tree.pretty_print
-tree.insert(5)
-tree.insert(6)
-tree.pretty_print
-tree.insert(7)
-tree.pretty_print
-tree.insert(8)
-tree.pretty_print
+puts tree.minimum_value
+puts tree.inorder_successor(tree.root.right)
