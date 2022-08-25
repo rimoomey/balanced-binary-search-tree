7e415a88022376800181cb434488ed6fe85eae9f
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-25 08:43:52 -0500

    Include and implement Comparable.

--- node.rb
+++ node.rb
@@ -2,11 +2,16 @@
 
 # Class for binary tree node--contains left and right branches
 class Node
+  include Comparable
   attr_accessor :value, :left, :right
 
   def initalize(value = nil, left = nil, right = nil)
     @value = value
     @left = left
     @right = right
+  end
+
+  def <=>(other)
+    @value <=> other.value
   end
 end
