5f0ef56082e16fea6cefc8a4f8417ba2b422c599
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-25 08:41:17 -0500

    Create node constructor.

--- node.rb
+++ node.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: false
 
 # Class for binary tree node--contains left and right branches
-class Node; end
+class Node
+  attr_accessor :value, :left, :right
+
+  def initalize(value = nil, left = nil, right = nil)
+    @value = value
+    @left = left
+    @right = right
+  end
+end
